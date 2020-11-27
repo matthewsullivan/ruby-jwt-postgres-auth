316815f792e55352c520981635b32a9698fbafd1
Matthew Sullivan <contact@matthewsullivan.media>
2020-11-26 20:50:35 -0500

    Add update_user test coverage

--- test/graph/mutations/update_user_test.rb
+++ test/graph/mutations/update_user_test.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-
+require 'graphql'
 module Mutations
   class UpdateUserTest < ActiveSupport::TestCase
-    setup do
-      login_as(users[0])
-    end
-
     def build_args
       {
         arguments: {
@@ -20,25 +16,54 @@ module Mutations
     end
 
     def perform(args = {})
-      User::Mutations::UpdateUser.new(object: nil, field: nil, context: {}).resolve(args)
+      result = login_as(users(:john))
+      context = {
+        session: { token: result[:token] },
+        current_user: result[:user]
+      }
+      User::Mutations::UpdateUser.new(object: nil, field: nil, context: context).resolve(args)
     end
 
     test 'should update user' do
       args = build_args
       result = perform(args)
+      user = result[:user]
 
-      assert result.persisted?
-      assert_equal result.first_name, user[:first_name]
-      assert_equal result.last_name, user[:last_name]
-      assert_equal result.email, user[:email]
+      assert_equal(user.first_name, user[:first_name])
+      assert_equal(user.last_name, user[:last_name])
+      assert_equal(user.email, user[:email])
     end
 
-    test 'should not register without first name' do
+    test 'should not update without first name' do
       exception = assert_raises ActiveRecord::RecordInvalid do
         perform(arguments: { first_name: '' })
       end
 
       assert_equal("Validation failed: First name can't be blank", exception.message)
+    end
+
+    test 'should not update without last name' do
+      exception = assert_raises ActiveRecord::RecordInvalid do
+        perform(arguments: { last_name: '' })
+      end
+
+      assert_equal("Validation failed: Last name can't be blank", exception.message)
+    end
+
+    test 'should not update without email' do
+      exception = assert_raises ActiveRecord::RecordInvalid do
+        perform(arguments: { email: '' })
+      end
+
+      assert_equal("Validation failed: Email can't be blank", exception.message)
+    end
+
+    test 'should not update with pre-existing email' do
+      exception = assert_raises ActiveRecord::RecordInvalid do
+        perform(arguments: { email: 'janedoe@localhost.com' })
+      end
+
+      assert_equal('Validation failed: Email has already been taken', exception.message)
     end
   end
 end
