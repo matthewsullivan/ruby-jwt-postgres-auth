16e4c3112a0f068ba6107c526ec2f86460b26957
Matthew Sullivan <contact@matthewsullivan.media>
2020-12-21 13:36:10 -0500

    Add user query

--- app/graph/base/types/query_type.rb
+++ app/graph/base/types/query_type.rb
@@ -2,6 +2,13 @@
 
 module Base::Types
   class QueryType < Base::Types::BaseObject
-    field :user, User::Types::UserType, null: false, description: 'An example field added by the generator'
+    field :user, User::Types::UserType, null: false, description: 'A registered user' do
+      argument :id, String, required: true
+    end
+
+    def user(id:)
+      _type, item_id = RubyJwtPostgresAuthSchema::object_from_id(id, nil)
+      User.find(item_id)
+    end
   end
 end
